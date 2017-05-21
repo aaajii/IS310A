b67f32b85aa863f4d1aac34b62a74a8478b3abb9
Jimuel Banawan <jwpbanawan@addu.edu.ph>
2017-05-21 15:23:41 +0800

    edited products

--- products/products.php
+++ products/products.php
@@ -1,15 +1,10 @@
 <?php
 
-	echo	"<div class='product'>
-			<h3> Basketball </h3>
-			<a href='add-to-cart.php?id=1'>add to cart </a>
-			</div>
-			<div class='product'>
-				<h3> Baseball </h3>
-				<a href='add-to-cart.php?id=2'>add to cart </a>
-			</div>
-			<div class='product'>
-				<h3> Football </h3>
-				<a href='add-to-cart.php?id=3'>add to cart </a>
-			</div>	";
+	while($result = $execute -> fetch_assoc())
+	{
+		echo "<div class='product'>
+				
+			  
+			  </div>";
+	}
 ?>
