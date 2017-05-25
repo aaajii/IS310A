91314047e37590f934b9545141a359cb2b7fc1a0
Jimuel Banawan <jwpbanawan@addu.edu.ph>
2017-05-25 11:09:21 +0800

    temp

--- products/add-to-cart.php
+++ products/add-to-cart.php
@@ -1,8 +1,18 @@
 <?php
-	if(isset($_POST[]))
+	if(isset($_POST['idList']) &&
+	   isset($_POST['nameList']) &&
+	   isset($_POST['totalList']) &&
+	   isset($_POST['quantityList']) &&
+	)
 	{
 		session_start();
 
+		
+		$_POST['idList']
+		$_POST['nameList']
+		$_POST['totalList']
+		$_POST['quantityList']
+		
 		if (empty($_SESSION['idList']) && 
 			empty($_SESSION['nameList']) &&
 			empty($_SESSION['totalList']) &&
