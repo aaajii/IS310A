824faaa7b0dd567e9d7d76d699254264feb51618
encastano <elton.castano@yahoo.com>
2017-05-26 00:24:47 +0800

    katung sa products & edited header

--- products/add-to-cart.php
+++ products/add-to-cart.php
@@ -1,23 +1,13 @@
 <?php
-	if(isset($_POST[]))
+	session_start();
+	
+	if (empty($_SESSION['cart']))
 	{
-		session_start();
-
-		if (empty($_SESSION['idList']) && 
-			empty($_SESSION['nameList']) &&
-			empty($_SESSION['totalList']) &&
-			empty($_SESSION['quantityList'])
-		   )
-		{
-			$_SESSION['idList'] = array();
-			$_SESSION['nameList'] = array();
-			$_SESSION['totalList'] = array();
-			$_SESSION['quantityList'] = array();
-		}
-		array_push($_SESSION['idList'], $row['itemID']);
-		array_push($_SESSION['nameList'], $row['itemName']);
-		array_push($_SESSION['totalList'], $_GET['price']);
-		array_push($_SESSION['quantityList'], $_GET['qty']);
-		echo "Item added to cart! <a href=''> Click here to view your cart! </a>";
+		$_SESSION['cart'] = array();
 	}
+	array_push($_SESSION['cart'], $_GET['id']);
 ?>
+<p>
+	product was added succesfully!
+	<a href="shopping-cart.php"> Click here to view your items</a>
+</p>
--- products/products.php
+++ products/products.php
@@ -1,13 +1,118 @@
 <!DOCTYPE html>
-
 <html>
-<head>
+  <head>
+    <title>Products</title>
+      <style>
+	       h1 {
+	       font-family: 'Crete Round', serif;
+	       font-weight: bold;
+	       color: blue;
+	       font-size: 65px;
+	      
+              }		
+		   h2 {
+				  font-family: 'Crete Round', serif;
+	       font-weight: bold;
+	       color: black;
+	       font-size: 40px;
+		   margin-left: 50px;
+			  }
+			 h4 {
+				  font-family: 'Crete Round', serif;
+	       font-weight: bold;
+	       color: black;
+	       font-size: 20px;
+			  }  
+			  
+        .wrapper {
+                	margin: 0 auto;
+                	padding: 0 10px;
+                	width: 940px;
+					
+               }
+         header {
+	              height: 100px;
+				  background-color: gray;
+				  opacity: 0.75;
+	            }
+
+         header h1 {
+	                 float: left;
+	                 margin-top: 1px;
+	                 margin-left: 50px;
+                   }
+		
+          header nav {
+	               float: right;
+                     }
+
+       
+		 .btn-group .button {
+    background-color: white;
+    border: none;
+    color: blue;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 17px;
+    cursor: pointer;
+    float: right;
+	margin-top: 40px;
+}
+.btn-group .buttonact {
+    background-color: black;
+    border: none;
+    color: blue;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 17px;
+    cursor: pointer;
+    float: right;
+	margin-top: 40px;
+}
+.btn-group .button:hover {
+    background-color: black;
+}
+.btn-group .button2{
+	float: center;
+	border-radius: 50%;
+	margin-top:15px;
+	margin-left:80px;
+}
+.shopcart{height:60px;width:60px;}
+
+#background{float: center; background-color: red;}
+#product{margin-left:225px;margin-right:225px;background-color: red;}
+</style>
 <link rel="stylesheet" type="text/css" href="productstyles.css"> 
+<link rel="shortcut icon" href="bytes-icon.ico" />
 </head>
 
 <body>
-<h2>Product list</h2>
-<h4>Browse items here</h4>
+
+    <header>
+	  <div class="wrapper">
+		<h1>4Bytes<span class="color"></span></h1>
+		
+		<div class="btn-group">
+		  <button class="button"><a href="newLogin.php">Log out</a></button>
+		  <button class="button"><a href="http://localhost:8080/IS310A/selling/sellform.html">Sell</a></button>
+          <button class="buttonact"><a href="http://localhost:8080/IS310A/products/products.php">Buy</a></button>         
+		  <button class="button"><a href="http://localhost:8080/IS310A/home%20page%204bytes/hp.html">Home</a></button>
+          <button class="button2"><a href=""><img class="shopcart " src="shop-cart.png" ></a></button>
+         
+          
+        </div>
+	  </div>
+	</header>
+	<div id="background">
+	<h2><p>Product List</p></h2>
+
+<div id="product">
+<h4>Browse items here:</h4>
 <?php
 	include 'config.php';
 	
@@ -37,19 +142,25 @@
 			$newDesc = $description;
 		}
 		
-		echo " <div class='responsive'>
-				  <div class='product'>
+		echo " <div class='responsive' >
+				  <div class='product'  >
+				  
+				  
 					<center><a href='productdetails.php?id=".$result['itemID']."'>
-					  <img src='".$result['itemImage']."' alt='1024px-No_image_available.svg.png' style='width:30%;height:auto;'>
+					
+					  <img src='".$result['itemImage']."' alt='".$result['itemName']."' style=''>
 					</a>
 					<p><span><b> Price: </b>".$result['itemPrice']." </span></p></center>
 					
-					<div class='desc'><b>".$result['itemName']."</b> <br/>
+					<div class='desc' ><b>".$result['itemName']."</b> <br/>
 									  ".$newDesc."
 					</div>
+				 
 				  </div>
 				</div>";
 	}
 ?>
+</div>
+</div>
 </body>
 </html>
